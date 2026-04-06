3e321f6e606c3c1c64e14aa426b62ac3953944c7
shaifa lenah <shaifalenah69@gmail.com>
2026-04-06 15:05:35 +0530

    feat: part2 routing, detail page, saved items

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://world.openfoodfacts.org',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
